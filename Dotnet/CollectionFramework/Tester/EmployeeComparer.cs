c5e44a068e4784aead00a0499dfa643028cefafd
ShubhamGudekar <gudekarshubham@gmail.com>
2023-01-05 21:19:56 +0530

    Collection framework in Dotnet

---
+++ Dotnet/CollectionFramework/Tester/EmployeeComparer.cs
@@ -0,0 +1,17 @@
+namespace TFLCollection;
+class EmpComparer:IComparer<Employee>{
+    public int Compare(Employee? e1, Employee? e2){
+        //int ret = e1.name.Length.CompareTo(e2.name.Length);
+        //return ret
+        // if(e1.salary > e2.salary){
+        //     return 1;
+        // }
+        // else if (e1.salary < e2.salary){
+        //     return -1;
+        // }
+        // else
+        // return 0;
+        return e1.salary.CompareTo(e2.salary);
+}
+
+}
